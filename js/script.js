48149c10fe13489eee30cd2278a83c22711301f1
PelekhIgor <igorpelekh0112@gmail.com>
2022-04-23 01:02:23 +0300

    шаблонизация

--- js/script.js
+++ js/script.js
@@ -3,25 +3,18 @@ const inpLastE = document.getElementById('input2')
 const inpTelE = document.getElementById('input3')
 const btnE = document.getElementById('btn')
 const listE = document.getElementById('list_container')
-
+const templateE = document.getElementById('template')
 btnE.addEventListener('click', onAddTodo)
+
 function onAddTodo (){
     const first = inpFirstE.value
     const last = inpLastE.value
     const tel = inpTelE.value
-    if(!first.trim() || !last.trim() || !tel.trim()){
+    if(!first.trim() || !last.trim() || !tel.trim() || isNaN(tel)){
         alert('please add some text')
         return
     }
 
-    if (!isNaN(first || last)){
-        alert('please enter your name')
-        return
-    }
-    if(isNaN(tel)){
-        alert('please enter phone number')
-        return
-    }
     const liE = createToDoE(first,last,tel)
     addElement(liE,listE)
     inpFirstE.value = ' '
@@ -29,25 +22,15 @@ function onAddTodo (){
     inpTelE.value = ' '
 }
 function createToDoE(first,last,tel){
-    const container = createElement('li')
-    const firstElem = createElement('div')
-    const lastElem = createElement('div')
-    const telElem = createElement('div')
-    container.classList.add('cont')
-    firstElem.textContent = first
-    lastElem.textContent = last
-    telElem.textContent = tel
-    addElement(firstElem , container)
-    addElement(lastElem , container)
-    addElement(tel , container)
-return container
+     const liE = templateE.innerHTML
+         .replace("{{first}}",first)
+         .replace("{{last}}",last)
+         .replace("{{tel}}",tel)
+return liE
 }
 
-function createElement(tag){
-    return document.createElement(tag)
-}
 function addElement(element, container){
-    container.append(element)
+    container.innerHTML += element
 }
 
 
